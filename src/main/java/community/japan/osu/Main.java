51e80d2f930e36df46ba45b14016b1776aefb638
mames1dev <e1b24017@oit.ac.jp>
2024-11-30 01:41:23 +0900

    Twitter機能削除

--- src/main/java/community/japan/osu/Main.java
+++ src/main/java/community/japan/osu/Main.java
@@ -1,7 +1,6 @@
 package community.japan.osu;
 
 import community.japan.osu.Object.Bot;
-import community.japan.osu.Object.Twitter;
 import community.japan.osu.Object.VoiceChat;
 import net.dv8tion.jda.api.JDA;
 
@@ -11,14 +10,12 @@ public class Main {
     public static Bot bot;
     public static JDA jda;
     public static VoiceChat voiceChat;
-    public static Twitter twitter;
 
     // 起動
     public static void main(String[] args) {
 
         bot = new Bot();
         voiceChat = new VoiceChat();
-        twitter = new Twitter();
 
         bot.loadJDA();
         jda = bot.getJda();
