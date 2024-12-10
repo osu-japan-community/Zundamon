477f0905682cb81610993dc015b9cc38b6b5e5d6
mames1dev <e1b24017@oit.ac.jp>
2024-12-11 01:28:09 +0900

    いらないです

--- src/main/java/community/japan/osu/Message/Leave.java
+++
@@ -1,9 +0,0 @@
-package community.japan.osu.Message;
-
-import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
-public class Leave extends ListenerAdapter {
-
-}
